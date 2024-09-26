dee8ab9109df716e06c6bb79f52e0c18c4453eee
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-26 21:13:37 +0500

    added the firebase from env

--- Config.js
+++ Config.js
@@ -1,3 +1,17 @@
 import dotenv from 'dotenv/config'
 const PORT = process.env.PORT
-export { PORT }
+const apiKey = process.env.FIREBASE_API_KEY
+const authDomain = process.env.FIREBASE_AUTH_DOMAIN
+const projectId = process.env.FIREBASE_PROJECT_ID
+const storageBucket = process.env.FIREBASE_STORAGE_BUCKET
+const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID
+const appId = process.env.FIREBASE_APP_ID
+export {
+  PORT,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+}
